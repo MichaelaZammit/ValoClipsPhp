b0a57e21656beb373639074877273df7eb460203
Michaela Zammit <michaela.zammit.f33220@mcast.edu.mt>
2024-05-29 20:49:22 +0200

    NavBar
    Created the NavBar and Styled it

---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>ValoClips</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <script defer src="button.js"></script>
+        <link rel="stylesheet" href="style/style1.css">
+    </head>
+    <body>
+
+<div class="container">
+    <ul class="nav nav-pills">
+      
+      <li class="nav-item">
+        <a class="nav-link <?php if ($CURRENT_PAGE == "Posts") { echo "active-button"; }?>" href="post.php">
+        <input type="image" id="image" alt="Post" src="images/post.png" width="auto" height="35">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?php if ($CURRENT_PAGE == "Challanges") { echo "active-button"; }?>" href="challanges.php">
+        <input type="image" id="image" alt="Challanges" src="images/challanges.png" width="auto" height="35">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?php if ($CURRENT_PAGE == "Home") { echo "active-button"; }?>" href="index.php">
+        <input type="image" id="image" alt="ValoClips" src="images/ValoClips.png" width="auto" height="35">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?php if ($CURRENT_PAGE == "Messages") { echo "active-button"; }?>" href="messages.php">
+        <input type="image" id="image" alt="Messages" src="images/messages.png" width="auto" height="35">
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?php if ($CURRENT_PAGE == "Profile") { echo "active-button"; }?>" href="profile.php">
+        <input type="image" id="image" alt="Profile" src="images/profile.png" width="auto" height="35">
+        </a>
+      </li>
+    </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include 'includes/header.php';
+?>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="well">
+                    <h1>ValoClips</h1>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6" style="left:0.5%;">
+                <img scr="images/ValoClips.png" class="img-round" title="Home">
+            </div>
+        </div>
+    </body>
+</html>
+
